d38d04ee65999e288544e386b1b87a05182d8657
Yakubu Tasiru <yakmtasir@gmail.com>
2025-09-14 19:16:57 +0100

    modified 2-argument

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const count = process.argv.lenght - 2;
+
+if (count === 0) {
+    console.log("No argument");
+}
+    else if (count === 1) {
+        console.log("Argument found");
+    }
+    else {
+    console.log("Arguments found");
+}
