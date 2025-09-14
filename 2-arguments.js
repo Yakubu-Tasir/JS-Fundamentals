d7f7994e2cd5363b4351996bc4f5e063f8c6354f
Yakubu Tasiru <yakmtasir@gmail.com>
2025-09-14 19:38:58 +0100

    modified 2-arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
-const args = process.argv.slice(2);
-const n = args.length;
+const args = process.argv.lenght - 2;
+
 
 if (n === 0) {
-  console.log('0 argument');
+  console.log("0 argument");
 } else if (n === 1) {
-  console.log('1 argument');
+  console.log("1 argument");
 } else {
   console.log(`${n} arguments`);
 }
