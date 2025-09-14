b7431009ac4a3ff96112aa985d0c2c1e7e3b300d
Yakubu Tasiru <yakmtasir@gmail.com>
2025-09-14 22:45:25 +0100

    modified 5-to_integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,9 +1,20 @@
-const arg = Process.argv[2];
-const num = parseInt(arg);
 
-if (isNaN(num)) {
-    console.log("Not a number");
-    else {
-        console.log("My number: " + num);
-    }
+const raw = process.argv[2];
+
+
+if (raw === undefined) {
+  console.log('argument:');
+  process.exit(0);
+}
+
+
+const cleaned = String(raw).replace(/^['"]|['"]$/g, '');
+
+
+const n = parseInt(cleaned, 10);
+
+if (isNaN(n)) {
+  console.log('Not a number');
+} else {
+  console.log('argument: ' + n);
 }
