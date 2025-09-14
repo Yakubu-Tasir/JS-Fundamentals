d752fb4458ad074734990eb8deea2af86e966553
Yakubu Tasiru <yakmtasir@gmail.com>
2025-09-14 22:59:36 +0100

    modified t-to_integrs

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,32 +1,8 @@
-#!/usr/bin/node
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
 
-const raw = process.argv[2];
-
-if (raw === undefined) {
-  // no argument provided
-  console.log('argument:');
-  process.exit(0);
-}
-
-// detect quoted forms (straight or common curly quotes)
-const first = raw[0];
-const last = raw[raw.length - 1];
-const isQuoted = (
-  (first === "'" && last === "'") ||
-  (first === '"' && last === '"') ||
-  (first === '‘' && last === '’') ||
-  (first === '“' && last === '”')
-);
-
-if (isQuoted) {
-  // preserve the quotes exactly as provided
-  console.log('argument: ' + raw);
+if (isNaN(num)) {
+  console.log('Not a number');
 } else {
-  // try to convert to number (accept floats, then use integer part)
-  const n = parseFloat(raw);
-  if (Number.isNaN(n)) {
-    console.log('Not a number');
-  } else {
-    console.log('argument: ' + parseInt(n, 10));
-  }
+  console.log('My number: ' + num);
 }
